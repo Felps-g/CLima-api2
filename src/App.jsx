c4919a37ac4fca286642ec8d641491cd29c54dbb
Felps-g <felipegianinni33@gmail.com>
2025-10-20 09:46:15 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,22 @@ function App() {
     setErro('');
 
     try{
+      const API_KEY = "";
+      const url = "";
+      const resposta = await fetch(url);
+
+      if (!resposta.ok){
+        throw new Error ('Cidade não encontrada');
+      }
+
+      const dados = await resposta.json();
+      setClima(dados);
 
     }catch (error){
-
+      setErro(error.message);
+      setClima(null);
     }finally{
-      
+      setCarregando(false);
     }
   }
   return (
