015d30d9c9a3bf0e97f599d8fc2054b239911c7a
Felps-g <felipegianinni33@gmail.com>
2025-10-27 09:43:49 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { CloudSun, MapPinned, Thermometer, Droplet, Wind } from 'lucide-react';
 
 function App() {
   const [cidade, setCidade] = useState('');
@@ -18,24 +19,112 @@ function App() {
     setCarregando(true);
     setErro('');
 
+    // Try Executa os comandos
     try{
+      const API_KEY = "5087";
+      const url = "";
+      const resposta = await fetch(url);
+
+      if(!resposta.ok){
+        throw new Error('Cidade não encontrada');
+      }
+
+      const dados = await resposta.json();
+      setClima(dados);
 
     }catch (error){
-
+      setErro(error.message);
+      setClima(null);
     }finally{
-      
+      setCarregando(false);
     }
   }
   return (
     <>
       <div className="container">
         <div className="content">
-          <hesder>
-            <h1>consulta de clima
-             <Camera color="red" size={48} />
+          <header>
+            <h1>
+              <CloudSun color="white" size={48} />
+              Consulta de Clima
             </h1>
             <p>Exemplo de consumo de API com React</p>
-          </hesder>
+          </header>
+          
+          {/* Caixa de Busca */}
+          <div className="busca-box">
+            <div className="busca-container">
+              <input 
+                type="text"
+                placeholder="Digite o nome da cidade.."
+              />
+              <button>Buscar</button>
+            </div>
+          </div>
+
+          {/* Resultado do Clima */}
+          <div id="card-resultado">
+            <div id="cidade-info">
+              <div id="cidade-nome">
+                <MapPinned style={{color: '#550808ff'}} size={48} />
+                Campinas, BR
+              </div>
+              <p id="cidade-desc">
+                Nublado
+              </p>
+            </div> {/* Fecha #cidade-desc*/}
+
+            {/* Temperatura principal */}
+            <div id="temperatura-box">
+              <div id="temp-valor">21°C</div>
+              <div id="temp-sens">
+                Sensação Térmica: 21°C
+              </div>
+            </div>
+
+            <div id="detalhes-box">
+              
+              <div className="detal-item">{/* Inicio Temperatura */}
+                <div className="detal-icone">
+                  <Thermometer />
+                </div>
+                <p className="detal-texto">
+                  Min/Max
+                </p>
+                <p className="detal-valor">
+                  23ºC/27ºC
+                </p>
+              </div>{/* Fim Temperatura */}
+
+              <div className="detal-item">{/* Inicio Umidade */}
+                <div className="detal-icone">
+                  <Droplet />
+                </div>
+                <p className="detal-texto">
+                  Umidade
+                </p>
+                <p className="detal-valor">
+                  12%
+                </p>
+              </div>{/* Fim Umidade */}
+
+              <div className="detal-item">{/* Inicio Vento */}
+                <div className="detal-icone">
+                  <Wind />
+                </div>
+                <p className="detal-texto">
+                  Vento
+                </p>
+                <p className="detal-valor">
+                  12 km/h
+                </p>
+              </div>{/* Fim Vento */}
+
+            </div>
+
+
+          </div> {/* Fecha #card-resultado */}
+
         </div>
       </div>
     </>
